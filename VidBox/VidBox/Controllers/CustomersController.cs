e08d89fa8a7dedc722ac4722a7be4b2f969df562
Tarun Reddy Gaddam <tarunreddy.04@gmail.com>
2018-06-22 14:57:01 -0500

    Added New and Edit views for Customer Updating data

--- VidBox/VidBox/Controllers/CustomersController.cs
+++ VidBox/VidBox/Controllers/CustomersController.cs
@@ -44,5 +44,36 @@ namespace VidBox.Controllers
             };
             return View(viewModel);
         }
+        // GET : Customers/Edit/{id}
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.Single(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new NewCustomerViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
     }
 }
