6fb538841f221f253c20edb7d01b1d4bbea25ed6
Tarun Reddy Gaddam <tarunreddy.04@gmail.com>
2018-06-21 23:40:11 -0500

    Added Initial View for Customers

--- VidBox/VidBox/Controllers/CustomersController.cs
+++ VidBox/VidBox/Controllers/CustomersController.cs
@@ -1,17 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using VidBox.Models;
 
 namespace VidBox.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
-            return View();
+            var customers = _context.Customers.ToList();
+            return View(customers);
+        }
+        // GET: Customers/Details/{id}
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
         }
     }
 }
