d47ea4f45f351e0a16593b6b2cc7f615f02ca3f7
Tarun Reddy Gaddam <tarunreddy.04@gmail.com>
2018-06-22 10:43:20 -0500

    Added Initial View for Movies

--- VidBox/VidBox/Controllers/MoviesController.cs
+++ VidBox/VidBox/Controllers/MoviesController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using VidBox.Models;
 
 namespace VidBox.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Customers
         public ActionResult Index()
         {
-            return View();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+            return View(movies);
+        }
+        // GET: Customers/Details/{id}
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
         }
     }
 }
