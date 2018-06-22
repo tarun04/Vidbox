25e4f0d5c594e4db0672b762eaac00ec8d02686f
Tarun Reddy Gaddam <tarunreddy.04@gmail.com>
2018-06-22 10:16:36 -0500

    Revert "Added Genre Model"
    This reverts commit ef9d17b18c44bc4db6ffc3f340a726fb05b7b576.

--- VidBox/VidBox/Controllers/MoviesController.cs
+++ VidBox/VidBox/Controllers/MoviesController.cs
@@ -3,38 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data.Entity;
-using VidBox.Models;
 
 namespace VidBox.Controllers
 {
     public class MoviesController : Controller
     {
-        private ApplicationDbContext _context;
-
-        public MoviesController()
-        {
-            _context = new ApplicationDbContext();
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            _context.Dispose();
-        }
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = _context.Movies.Include(m => m.Genre).ToList();
-            return View(movies);
-        }
-        // GET: Movies/Details/{id}
-        public ActionResult Details(int id)
-        {
-            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
-
-            if (movie == null)
-                return HttpNotFound();
-            return View(movie);
+            return View();
         }
     }
 }
