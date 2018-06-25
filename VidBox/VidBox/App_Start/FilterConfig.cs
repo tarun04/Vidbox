67c8c0d0a699f4daa9dc4b484ffd656b6ea95812
Tarun Reddy Gaddam <tarunreddy.04@gmail.com>
2018-06-25 18:43:01 -0500

    Added Authorization

--- VidBox/VidBox/App_Start/FilterConfig.cs
+++ VidBox/VidBox/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace VidBox
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
