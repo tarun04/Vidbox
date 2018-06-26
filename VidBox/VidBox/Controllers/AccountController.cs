76f4b18cbe9a0c5a399b6cb0ad35e91da0b78992
Tarun Reddy Gaddam <tarunreddy.04@gmail.com>
2018-06-25 21:52:20 -0500

    Added Phone to Register

--- VidBox/VidBox/Controllers/AccountController.cs
+++ VidBox/VidBox/Controllers/AccountController.cs
@@ -152,7 +152,7 @@ namespace VidBox.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Phone = model.Phone, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
