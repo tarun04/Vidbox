56dfb1326aec17543f112016444aa19ad58fc16f
Tarun Reddy Gaddam <tarunreddy.04@gmail.com>
2018-06-25 20:58:27 -0500

    Added Phone to Register Users

--- VidBox/VidBox/Models/AccountViewModels.cs
+++ VidBox/VidBox/Models/AccountViewModels.cs
@@ -8,6 +8,10 @@ namespace VidBox.Models
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Phone { get; set; }
     }
 
     public class ExternalLoginListViewModel
@@ -68,6 +72,10 @@ namespace VidBox.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Phone { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
